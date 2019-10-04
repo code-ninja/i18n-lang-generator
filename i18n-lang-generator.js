6de30d4e9af6e0504231f43b9e826b3a8d833bb1
Paulo Marinas <paulopmx@gmail.com>
2019-10-04 12:53:09 +0800

    add eslint

--- i18n-lang-generator.js
+++ i18n-lang-generator.js
@@ -5,28 +5,27 @@ const glob = require('glob')
 const _ = require('lodash')
 const sortObject = require('deep-sort-object')
 
-class i18nLangGenerator {
+class I18nLangGenerator {
   constructor(options) {
-    this.options = _.extend({
-      base: '',
-      from: [],
-      to: '',
-      languages: [],
-      extensions: ['vue', 'js'],
-      functionName: '\\$t',
-      deleteExpired: false,
-      forceReWrite: false
-    }, options)
+    this.options = _.extend(
+      {
+        base: '',
+        from: [],
+        to: '',
+        languages: [],
+        extensions: ['vue', 'js'],
+        functionName: '\\$t',
+        deleteExpired: false,
+        forceReWrite: false
+      },
+      options
+    )
   }
 
   run() {
-    const {
-      from,
-      extensions,
-      base
-    } = this.options
-    let path = from.join('|')
-    let ext = extensions.join('|')
+    const { from, extensions, base } = this.options
+    const path = from.join('|')
+    const ext = extensions.join('|')
     glob(`${base}/@(${path})/**/*.@(${ext})`, {}, (err, files) => {
       if (err) throw err
       this.writeJSON(this.getText(files))
@@ -35,43 +34,51 @@ class i18nLangGenerator {
 
   getText(files) {
     const fn = '\\$t'
-    let obj = {}
+    const obj = {}
 
     const createPath = (obj, path) => {
-
-      let orgPath = path
-      path = typeof path === 'string' ? path.split('.') : path;
+      const orgPath = path
+      path = typeof path === 'string' ? path.split('.') : path
       let current = obj
 
       while (path.length > 1) {
-
         const [head, ...tail] = path
         path = tail
-        if (current[head] === undefined)
+        if (current[head] === undefined) {
           current[head] = {}
+        }
         current = current[head]
-
       }
 
-      if (!current[path[0]])
+      if (!current[path[0]]) {
         current[path[0]] = path[path.length - 1]
-      else if (typeof current[path[0]] === 'object')
-        throw ({ message: `Cannot create a string ${orgPath} exist as an object` })
+      } else if (typeof current[path[0]] === 'object') {
+        throw Object.assign(
+          new Error(`Cannot create a string ${orgPath} exist as an object`),
+          { code: 402 }
+        )
+      }
 
       return obj
-
     }
 
-    files.forEach((file) => {
+    files.forEach(file => {
       const text = fs.readFileSync(file, 'utf8')
-      const findTranslations = new RegExp(`\\W${fn}\\(\\'([^\\']*)\\'(\\)|,)`, 'g')
+      const findTranslations = new RegExp(
+        `\\W${fn}\\(\\'([^\\']*)\\'(\\)|,)`,
+        'g'
+      )
       let result
-      let newObj = {}
-      while (result = findTranslations.exec(text)) {
+
+      // eslint-disable-next-line no-cond-assign
+      while ((result = findTranslations.exec(text))) {
         try {
           createPath(obj, result[1])
         } catch (e) {
-          console.log(`Error creating property ${result[1]}. ${e.message}`, file)
+          console.log(
+            `Error creating property ${result[1]}. ${e.message}`,
+            file
+          )
           process.exit(1)
         }
       }
@@ -81,26 +88,17 @@ class i18nLangGenerator {
   }
 
   writeJSON(result) {
+    const { languages } = this.options
 
-    const {
-      languages
-    } = this.options
-
-    languages.forEach((lang) => {
+    languages.forEach(lang => {
       this.processLanguage(lang, result)
     })
-
   }
 
   processLanguage(lang, result) {
-
     console.log(`\n${lang}.json`)
 
-    const {
-      base,
-      to,
-      deleteExpired
-    } = this.options
+    const { base, to, deleteExpired } = this.options
 
     const localeText = this.getLocaleConfig(lang)
 
@@ -110,92 +108,90 @@ class i18nLangGenerator {
     let itemDeleted = false
     let itemAdded = false
 
-    localeMap.forEach((item) => {
-
+    localeMap.forEach(item => {
       if (resultMap.indexOf(item) < 0) {
-
         if (deleteExpired) {
           this.deletePropertyPath(localeText, item)
           itemDeleted = true
+        } else {
+          report[item] = 'unused'
         }
-        else
-          report[item] = "unused"
-
       }
-
     })
 
-    if (itemDeleted)
+    if (itemDeleted) {
       this.clean(localeText)
+    }
 
-    resultMap.forEach((item) => {
+    resultMap.forEach(item => {
       if (localeMap.indexOf(item) < 0) {
-        report[item] = "new item added"
+        report[item] = 'new item added'
         itemAdded = true
       }
     })
 
     const mergedObj = sortObject(_.merge({}, result, localeText))
 
-    if (itemAdded || itemDeleted)
+    if (itemAdded || itemDeleted) {
       fs.writeFileSync(
         `${base}/${to}/${lang}.json`,
         JSON.stringify(mergedObj, null, 2),
         'utf8'
       )
+    }
 
     const iterate = (obj, parent = '') => {
       Object.keys(obj).forEach(key => {
-        let newParent = parent ? parent + '.' + key : key
-        if (typeof obj[key] === 'object')
+        const newParent = parent ? parent + '.' + key : key
+        if (typeof obj[key] === 'object') {
           iterate(obj[key], newParent)
-        else if (key === obj[key] && !report[newParent])
+        } else if (key === obj[key] && !report[newParent]) {
           report[newParent] = 'needs translation'
+        }
       })
     }
 
     iterate(mergedObj)
 
-    if (Object.keys(report).length)
+    if (Object.keys(report).length) {
       console.table(report)
-    else
+    } else {
       console.log('No issues')
-
+    }
   }
 
   deletePropertyPath(obj, path) {
-
-    if (!obj || !path)
+    if (!obj || !path) {
       return
+    }
 
-    if (typeof path === 'string')
+    if (typeof path === 'string') {
       path = path.split('.')
+    }
 
     for (let i = 0; i < path.length - 1; i++) {
       obj = obj[path[i]]
-      if (typeof obj === 'undefined')
+      if (typeof obj === 'undefined') {
         return
+      }
     }
 
     delete obj[path.pop()]
-
   }
 
   clean(o) {
     for (var propName in o) {
-      if (typeof o[propName] == "object")
+      if (typeof o[propName] === 'object') {
         this.clean(o[propName])
-      if (!Object.keys(o[propName]).length)
-        delete o[propName];
+      }
+      if (!Object.keys(o[propName]).length) {
+        delete o[propName]
+      }
     }
   }
 
   getLocaleConfig(language) {
-    const {
-      base,
-      to,
-      forceReWrite
-    } = this.options
+    const { base, to, forceReWrite } = this.options
     const path = `${base}/${to}/${language}.json`
 
     try {
@@ -206,36 +202,37 @@ class i18nLangGenerator {
         return {}
       }
     } catch (error) {
-      console.warn(`${path}: Please fix`, error.message, `or force to rewrite the file with the option "-r true"`)
-      if (forceReWrite)
+      console.warn(
+        `${path}: Please fix`,
+        error.message,
+        'or force to rewrite the file with the option "-r true"'
+      )
+      if (forceReWrite) {
         return {}
-      else
+      } else {
         process.exit(1)
+      }
     }
   }
 
-
   flatten(value) {
-
-    let arr = []
+    const arr = []
 
     const iterate = (obj, parent = '') => {
       Object.keys(obj).forEach(key => {
-        let newParent = parent ? parent + '.' + key : key
-        if (typeof obj[key] === 'object')
+        const newParent = parent ? parent + '.' + key : key
+        if (typeof obj[key] === 'object') {
           iterate(obj[key], newParent)
-        else
+        } else {
           arr.push(newParent)
+        }
       })
     }
 
     iterate(value)
 
     return arr
-
   }
-
-
 }
 
 const argv = require('minimist')(process.argv.slice(2))
@@ -247,7 +244,7 @@ const outputDirectory = argv.o || argv.output || 'lang'
 const deleteExpired = argv.x || argv.deleteExpired || false
 const forceReWrite = argv.r || argv.forceReWrite || false
 
-new i18nLangGenerator({
+new I18nLangGenerator({
   base: baseDir.replace(/\/$/, ''),
   from: dir ? dir.split(' ') : [],
   to: outputDirectory,
